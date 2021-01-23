37a48236da1aefcfffe13f9abc922008dbe1e90f
roro1053 <c1053rroro@gmail.com>
2021-01-23 21:30:10 +0900

    投稿が保存される

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,8 +12,8 @@ class RecipesController < ApplicationController
   end
 
   def create
-    @recipe = Recipe.new
-    @recipe.user_id = current_user_id
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user_id = current_user.id
     if @recipe.save
       redirect_to recipe_path(@recipe), notice: '投稿に成功しました。'
     else
